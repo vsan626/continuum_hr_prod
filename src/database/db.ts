e56a7dc6af25c4ab9c48729469032c5a8f16eda2
Danny (vai) San <vai.quoc.san@gmail.com>
2021-09-14 22:09:46 -0700

    update eslintrc.js to .json

--- src/database/db.ts
+++ src/database/db.ts
@@ -1,5 +1,6 @@
 import { ConnectOptions, connect } from 'mongoose';
 
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 export default async () => {
   try {
     const connection = await connect('mongodb://localhost:27017/continuum', {
